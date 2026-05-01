7d4fd36aba2e95d3f7eced83a74df757911f7f47
boshrajo0-hub <boshrajo0@gmail.com>
2026-05-01 13:16:24 +0300

    Update ValidationHelper.cs

--- Helpers/ValidationHelper.cs
+++ Helpers/ValidationHelper.cs
@@ -2,17 +2,17 @@
 
 namespace DeliverySystemProject.Helpers
 {
-    // لاحظي كلمة static.. معناها هاد الكلاس "جاهز للاستخدام" فوراً
+
     public static class ValidationHelper
     {
-        // دالة بتتأكد إنو النص مو فاضي (مثلاً رقم التتبع أو العنوان)
+        // دالة تأكيد أن النص غير فارغ
         public static bool IsValidString(string text)
         {
-            // بترجع true إذا كان النص فيه قيمة، و false إذا كان فاضي
+            
             return !string.IsNullOrWhiteSpace(text);
         }
 
-        // دالة بتتأكد إنو الوزن مو صفر أو رقم سالب
+        // دالة تأكيد أن الوزن أكبر من الصفر
         public static bool IsValidWeight(double weight)
         {
             return weight > 0;
